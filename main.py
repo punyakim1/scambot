b72b295c736b9d141a2250abaf7aa136cfba5521
punyakim1 <126011201+punyakim1@users.noreply.github.com>
2023-03-27 12:07:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -210,8 +210,8 @@ async def kesatu(_, callback_query):
                 InlineKeyboardButton("𝘊𝘩𝘢𝘯𝘯𝘦𝘭 𝘍𝘰𝘶𝘳 🩸", url="https://t.me/+w59Fy6HRofJiZmE9"),
             ],
             [
-                InlineKeyboardButton("𝘊𝘩𝘢𝘯𝘯𝘦𝘭 𝘍𝘰𝘶𝘳 🩸", url="https://t.me/+w59Fy6HRofJiZmE9"),
-                InlineKeyboardButton("𝘊𝘩𝘢𝘯𝘯𝘦𝘭 𝘍𝘰𝘶𝘳 🩸", url="https://t.me/+w59Fy6HRofJiZmE9"),
+                InlineKeyboardButton("𝘊𝘩𝘢𝘯𝘯𝘦𝘭 𝘍𝘪𝘷𝘦 🩸", url="https://t.me/+ot-eZOIdhh04ZjE9"),
+                InlineKeyboardButton("𝘊𝘩𝘢𝘯𝘯𝘦𝘭 𝘚𝘪𝘹 🩸", url="https://t.me/+d6gKaT9iAR42Yzk1"),
             ]    
         ]
         await callback_query.edit_message_text(
