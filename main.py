e05d2a62aacc0bd20bb51fafd95dd822fefbbbd0
punyakim1 <126011201+punyakim1@users.noreply.github.com>
2023-03-27 11:42:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -204,6 +204,7 @@ async def kesatu(_, callback_query):
             [
                 InlineKeyboardButton("𝘊𝘩𝘢𝘯𝘯𝘦𝘭 𝘖𝘯𝘦 🩸", url="https://t.me/+3cmLG1Nc42UxNjI1"),
                 InlineKeyboardButton("𝘊𝘩𝘢𝘯𝘯𝘦𝘭 𝘛𝘸𝘰 🩸", url="https://t.me/+Kh7-WpPVb3w5Njk1"),
+                InlineKeyboardButton("𝘊𝘩𝘢𝘯𝘯𝘦𝘭 𝘍𝘰𝘶𝘳 🩸", url="https://t.me/+w59Fy6HRofJiZmE9")
             ],
             [
                 InlineKeyboardButton("𝘊𝘩𝘢𝘯𝘯𝘦𝘭 𝘛𝘩𝘳𝘦𝘦 🩸", url="https://t.me/+NjYTRB4tj0AxMDhl"),
